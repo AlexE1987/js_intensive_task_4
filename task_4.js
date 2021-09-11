6db3c938c903d4eb628f52a597f5dcbddd91fc79
AlexE1987 <evsikovad1987@gmail.com>
2021-09-11 16:28:27 +0400

    init: task_4; feat: implement class Calculator

---
+++ task_4.js
@@ -0,0 +1,33 @@
+class Calculator {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.args = [...arguments];
+    if (this.args.length !== 2 || this.args.some((e) => !Number.isFinite(e)))
+      throw new Error('Invalid number of parameters or value');
+  }
+  validateMethod(value) {
+    if (!Number.isFinite(value)) throw new Error('Invalid parameter value');
+  }
+  setX = (value) => {
+    this.validateMethod(value);
+    this.x = value;
+  };
+  setY = (value) => {
+    this.validateMethod(value);
+    this.y = value;
+  };
+  logSum = () => {
+    return this.x + this.y;
+  };
+  logMul = () => {
+    return this.x * this.y;
+  };
+  logSub = () => {
+    return this.x - this.y;
+  };
+  logDiv = () => {
+    if (this.y === 0) throw new Error('Value of `y` must not be 0');
+    return this.x / this.y;
+  };
+}
