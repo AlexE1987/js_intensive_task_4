bdb035285b936397e53833b99c9a5e5b1e204859
AlexE1987 <evsikovad1987@gmail.com>
2021-09-14 14:14:29 +0400

    refactor: add log into methods in class Calculator

--- task_4.js
+++ task_4.js
@@ -1,21 +1,36 @@
-const concatStrings = (string, separator) => {
-  let sumOfStrings = [string];
-
-  const additionalString = (nextString) => {
-    if (Number.isFinite(nextString) || typeof nextString === 'bigint') {
-      return sumOfStrings.join(separator || '');
-    }
-
-    if (nextString || nextString === '') {
-      sumOfStrings.push(nextString);
-      return additionalString;
-    }
-
-    return sumOfStrings.join(separator || '');
+class Calculator {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.args = [...arguments];
+    if (this.args.length !== 2 || this.args.some((e) => !Number.isFinite(e)))
+      throw new Error('Invalid number of parameters or value');
+  }
+  validateMethod(value) {
+    if (!Number.isFinite(value)) throw new Error('Invalid parameter value');
+  }
+  setX = (value) => {
+    this.validateMethod(value);
+    this.x = value;
   };
-
-  return additionalString;
-};
+  setY = (value) => {
+    this.validateMethod(value);
+    this.y = value;
+  };
+  logSum = () => {
+    console.log(this.x + this.y);
+  };
+  logMul = () => {
+    console.log(this.x * this.y);
+  };
+  logSub = () => {
+    console.log(this.x - this.y);
+  };
+  logDiv = () => {
+    if (this.y === 0) throw new Error('Value of `y` must not be 0');
+    console.log(this.x / this.y);
+  };
+}
 
 class Calculator {
   constructor(x, y) {
